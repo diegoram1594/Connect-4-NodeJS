0f1659e6a84c3e4b3a56e9dc8f21d1aa3d42e468
Your Name <you@example.com>
2019-04-21 12:20:37 -0500

    board, winner ,tie

--- index.js
+++ index.js
@@ -5,25 +5,207 @@ const io = require('socket.io')(server);
 
 let port=3000;
 
-let gameStatus="Preparing"
-const connections = new Map();
+//Init board
+let newBoard= new Array(6);
+for (let i = 0; i < 6; i++) {
+    newBoard[i] = new Array(7);
+}
 
+let game={
+    gameStatus :"Preparing",
+    player1: {
+        name:null,
+        lock:false
+    },
+    player2: {
+        name:null,
+        lock:false
+    },
+    turn:"p1",
+    board:newBoard
+}
+let statusNewGame=JSON.parse(JSON.stringify(game));
+
+const connections = new Map();
+let player1Connection=null;
+let player2Connection=null;
 app.use(express.static('public'));
 
 io.on('connection',(socket)=>{
     console.log("Someone is connected with sockets");
-    socket.emit("gameStatus",gameStatus);
+    socket.emit("gameStatus",game);
     connections.set(socket, socket);
 
 
+    //New player
+    socket.on('newPlayer',(number,name)=>{
+        console.log(`new player ${name} ${number}`)
+        if(number=='p1'){
+            game.player1.name=name;
+            player1Connection=socket;
+            game.player1.lock=true;
+        }
+        if(number=='p2'){
+            game.player2.name=name;
+            player2Connection=socket;
+            game.player2.lock=true;
+        }
+        if(game.player1.lock && game.player2.lock){
+            game.gameStatus="Playing";
+            game.turn="p1";
+
+        }
+        connections.forEach((value,key,map)=>{
+            value.emit("gameStatus",game);
+        });
+    });
+
+    //Turn Player
+    socket.on('turn',(player, line)=>{
+        console.log(`turn ${player.name} in ${line}`);
+        var color="";
+        if(player.number==='p1'){
+            game.turn="p2";
+            color="R"
+        }
+        if(player.number==='p2'){
+            game.turn="p1";
+            color="Y"
+        }
+        let i=0
+        for(i=5;i>=0;i--){
+            if(game.board[i][line]==null){
+                game.board[i][line]=color;
+                break;
+            }
+        }
+
+        //verify win
+        let win=false;
+        //Left
+        let verLeft=line-1;
+        let cont=0;
+        while(verLeft>=0 && game.board[i][verLeft]==color){
+            cont++;
+            verLeft--;
+        }
+        let verRight=line+1;
+        while(verRight<=6 && game.board[i][verRight]==color){
+            cont++;
+            verRight++;
+        }
+        if(cont>=3){
+            //Winner
+            win=true;
+        }else{
+            //Up
+            let verUp=i-1;
+            cont=0;
+            while(verUp>=0 && game.board[verUp][line]==color){
+                cont++;
+                verUp--;
+            }
+            //Down
+            let verDown=i+1;
+            while(verDown<=5 && game.board[verDown][line]==color){
+                cont++;
+                verDown++;
+            }
+            if(cont>=3){
+                //Winner
+                win=true;
+            }else{
+                //Diagonal Left
+                let verLineLeft=line-1;
+                let verColLeft=i-1;
+                cont=0;
+                while(verLineLeft>=0 && verColLeft>=0 && game.board[verColLeft][verLineLeft]==color){
+                    verLineLeft--;
+                    verColLeft--;
+                    cont++;
+                }
+                let verLineRight=line+1;
+                let verColRight=i+1;
+                while(verLineRight<=6 && verColRight<=5 && game.board[verColRight][verLineRight]==color){
+                    verLineRight++;
+                    verColRight++;
+                    cont++;
+                }
+                if (cont>=3){
+                    //Winner
+                    win=true;
+                }else{
+                     //Diagonal Right
+                    verLineLeft=line-1;
+                    verColLeft=i+1;
+                    cont=0;
+                    while(verLineLeft>=0 && verColLeft<=5 && game.board[verColLeft][verLineLeft]==color){
+                        verLineLeft--;
+                        verColLeft++;
+                        cont++;
+                    }
+                    verLineRight=line+1;
+                    verColRight=i-1;
+                    while(verLineRight<=6 && verColRight>=0 && game.board[verColRight][verLineRight]==color){
+                        verLineRight++;
+                        verColRight--;
+                        cont++;
+                    }if (cont>=3){
+                        //Winner
+                        win=true;
+                    }
+                }
+            }
+        }
+
+
+        
+        if (!win){
+            //Verify Tie
+            var tie=true;
+            for (var ii = 0; ii < 6; ii++) {
+                for (var j=0;j<7;j++){
+                  if(game.board[ii][j]==null){
+                    tie=false;
+                  }
+                }
+            }
+
+            if(tie){
+                connections.forEach((value,key,map)=>{
+                    value.emit("tie",game);
+                });
+                game=JSON.parse(JSON.stringify(statusNewGame));
+            }else{
+                connections.forEach((value,key,map)=>{
+                    value.emit("gameStatus",game);
+                });
+            }            
+        }else{
+            connections.forEach((value,key,map)=>{
+                value.emit("win",game,player.name);
+            }); 
+            game=JSON.parse(JSON.stringify(statusNewGame));
+        }
+    })
+
+    //Any user disconnect
     socket.on('disconnect', function(){
         connections.delete(socket)
         gameStatus="Disconnect";
         console.log("DISCONECT");
-        connections.forEach((value,key,map)=>{
-            value.emit("gameStatus",gameStatus);
-        });
-        gameStatus="Preparing";
+        
+
+        if(socket===player1Connection || socket===player2Connection){
+            //Unlock players
+            game=JSON.parse(JSON.stringify(statusNewGame));
+            console.log("Player disconnected");
+            connections.forEach((value,key,map)=>{
+                value.emit("disconnectPlayer",game);
+                //connections.delete(value)
+            });
+        }
+
       });
 });
 
--- public/main.js
+++ public/main.js
@@ -1,22 +1,131 @@
-var socket = io.connect('http://localhost:3000', { 'forceNew': true });
+var socket = io.connect();
 
+var player={
+  name:null,
+  number:null
+}
+
+//Init board
+var board= new Array(6);
+for (var i = 0; i < 6; i++) {
+  board[i] = new Array(7);
+}
+
+var localGame=null;
 socket.on('gameStatus', function(data) {
   console.log(data);
-  if (data=='Preparing'){
-    render(data);
-  }
-  if(data=='Disconnect'){
-    alert("Other Player Disconnected");
-  }
-})
+    renderGameStatus(data,false);
+});
 
-function render (data) {
+socket.on('disconnectPlayer', function(data) {
+  console.log(data);
+  alert("A player left the Game");
+  renderGameStatus(data,true);
+});
+
+socket.on('win', function(data,name) {
+    console.log(data);
+    renderGameStatus(data,true);
+    alert(name+" Win");
+    player={
+      name:null,
+      number:null
+    };
+});
+
+socket.on('tie', function(data,name) {
+  console.log(data);
+  renderGameStatus(data,true);
+  alert("Tie!");
+  player={
+    name:null,
+    number:null
+  };
+});
+
+function newPlayer(number){
+  var playerName=document.getElementById('playerName').value;
+  socket.emit("newPlayer",number,playerName);
+  player.name=playerName;
+  player.number=number;
+}
+
+
+function renderGameStatus (data,win) {
+  localGame=data;
+  if (win){
+    data.player1.name="<i>Wating for player...</i>";
+    data.player2.name="<i>Wating for player...</i>";
+    data.player1.lock=false;
+    data.player2.lock=false;
+  }else{
+    if(!!player.number){
+      data.player1.lock=true;
+      data.player2.lock=true;
+    }
+
+    if(data.player1.name==null){
+      data.player1.name="<i>Wating for player...</i>";
+    }
+    if (data.player2.name==null){
+      data.player2.name="<i>Wating for player...</i>";
+    }
+  }
   var html = (`<div>
-              <strong>test</strong>:
-              <em>test</em>
+              <strong>Player 1</strong>:
+              <em>${data.player1.name}</em>
+              <br>
+              <strong>Player 2</strong>:
+              <em>${data.player2.name}</em>
             </div>`);
 
-  document.getElementById('messages').innerHTML = html;
+    document.getElementById('gameStatus').innerHTML = html;
+    document.getElementById("btnPlayer1").disabled = data.player1.lock;
+    document.getElementById("btnPlayer2").disabled = data.player2.lock;
+
+    if(win){
+      document.getElementById("nameTurn").innerHTML="<h3>Game Over, Select player</h3>"; 
+    }else if (data.gameStatus==='Playing' ){
+      if( data.turn===player.number){
+          document.getElementById("nameTurn").innerHTML="<h3>Your Turn!</h3>";   
+        }else{
+          document.getElementById("nameTurn").innerHTML="<h3>Opponent Turn</h3>"; 
+        }
+      }else{
+        document.getElementById("nameTurn").innerHTML="" ;
+      }
+
+    var boardHtml="<table>";
+    for (var i = 0; i < 6; i++) {
+      boardHtml+="<tr>";
+      for (var j=0;j<7;j++){
+        boardHtml+="<th>";
+        if(data.board[i][j]==null){
+          boardHtml+=`<span class='dotWhite'onclick=selection(${i},${j})></span>`;
+        }else if(data.board[i][j]==='R'){
+          boardHtml+=`<span class='dotRed'></span>`;
+        }else if(data.board[i][j]==='Y'){
+          boardHtml+=`<span class='dotYellow'></span>`;
+        }
+        
+        boardHtml+="</th>";
+      }
+      boardHtml+="</tr>";
+    }
+    boardHtml+="</table>";
+
+    document.getElementById('gameBoard').innerHTML = boardHtml;
+
+    
+  
+}
+
+function selection(i,j){
+  console.log(i,j);
+  console.log(localGame);
+  if (!!player.name && localGame.gameStatus==='Playing' && localGame.turn===player.number){
+    socket.emit('turn',player,j);
+  }
 }
 
 function addMessage(e) {
@@ -27,4 +136,5 @@ function addMessage(e) {
 
   socket.emit('mensajeTest', message);
   return false;
+  
 }
